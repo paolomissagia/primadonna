1797647c734eadc044f1c5a4b1d8509f94ddb3ab
Paolo Missagia <login@paolomissagia.com>
2023-07-11 20:03:36 +0100

    removed the css file that was deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import './App.css';
+import About from './components/About';
+import Catalogue from './components/Catalogue';
+import Hero from './components/Hero';
+import Operas from './components/Operas';
 
 function App() {
   return (
     <div className="app-container">
+      <Hero />
+      <About />
+      <Catalogue />
+      <Operas />
     </div>
   );
 }
