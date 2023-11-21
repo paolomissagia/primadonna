9f177e3a4491ba91bca39b9843194734b23f8b33
Paolo Missagia <login@paolomissagia.com>
2023-11-21 12:41:55 +0000

    moved data folder and added operas

---
+++ src/assets/data/Features.ts
@@ -0,0 +1,13 @@
+interface Feature {
+  name: string,
+  description: string
+}
+
+export const features: Feature[] = [
+  { name: "origin", description: "italy" },
+  { name: "creation", description: "sixteenth century" },
+  { name: "format", description: "music, performing and singing" },
+  { name: "duration", description: "usually two-three hours" },
+  { name: "personalities", description: "pavarotti & maria callas" },
+  { name: "roles", description: "tenor, soprano, baritone" },
+];
---
+++ src/assets/data/Navigation.ts
@@ -0,0 +1,10 @@
+interface Navigation {
+  id: string,
+  name: string
+}
+
+export const navigation: Navigation[] = [
+  { id: "home", name: "home" },
+  { id: "about", name: "about" },
+  { id: "catalogue", name: "catalogue" },
+];
